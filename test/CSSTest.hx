caecf31ff2b5045d766911a83eaeda49015f7547
yannick <yannick.dominguez@gmail.com>
2014-05-29 22:58:33 +0200

    initial commit

---
+++ test/CSSTest.hx
@@ -0,0 +1,19 @@
+package;
+
+import cocktail.core.css.CSSData;
+import cocktail.core.css.CSSConstants;
+
+class CSSTest
+{
+    public function new()
+    {
+
+    }
+
+    @Test
+    public function testCSS()
+    {
+        var type = new TypedPropertyVO();
+    }
+}
+
